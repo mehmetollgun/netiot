cef5b12b76b47250f1ad41cb1d4bee116beaebc6
mehmetollgun <mehmetollgun@gmail.com>
2017-03-06 00:10:03 +0200

    Arduino

---
+++ arduino/receiver/receiver.ino
@@ -0,0 +1,39 @@
+#include <VirtualWire.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+String kelime;
+int sayac=0;
+ char harf[32];
+char*mesaj;
+
+void setup() {
+   Serial.begin(9600);
+    vw_set_ptt_inverted(true);
+    vw_set_rx_pin(7);
+    vw_setup(4000);
+    vw_rx_start();
+lcd.clear();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if(vw_get_message(buf, &buflen))
+    {
+        for(int i=0;i<buflen;i++)
+        {
+          harf[i]=buf[i];
+            sayac++;
+
+        }
+        lcd.begin(16,2);
+lcd.clear();
+
+        for(int j=0;j<sayac-1;j++)
+        {
+          lcd.print(harf[j]);
+        }
+    }
+}
---
+++ arduino/transmitter/transmitter.ino
@@ -0,0 +1,54 @@
+#include <VirtualWire.h>
+char mesaj[32];
+String kelime;
+int sayac=0;
+ char harf[32];
+void setup() {
+  // verici
+Serial.begin(9600);
+  vw_set_ptt_inverted(true);
+  vw_set_tx_pin(7);
+  vw_setup(4000);
+}
+void islem()
+{
+   int gelen=Serial.read();
+
+
+
+  harf[sayac]=gelen;
+ 
+
+
+  kelime+=harf[sayac];
+ 
+ 
+
+ mesaj[sayac]+=harf[sayac];
+ sayac++;
+vw_send((uint8_t *)mesaj,strlen(mesaj));
+vw_wait_tx();
+
+
+
+}
+void loop() {
+  // mehmet olgun
+
+
+
+
+if(Serial.available())
+{
+islem();
+ 
+delay(500);
+
+}
+else
+{
+   kelime="";
+   
+}
+
+}
